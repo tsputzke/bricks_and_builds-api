4f794c44648c38da8eeec95a35de90932e977e60
Timothy Putzke <tsputzke@gmail.com>
2020-01-12 19:15:20 -0500

    Implement full migration files

--- seeds/seed.bricks_and_builds_tables.sql
+++ seeds/seed.bricks_and_builds_tables.sql
@@ -10,12 +10,20 @@ INSERT INTO bricks_and_builds_users (user_name, password)
 VALUES
   ('TestUser', '$2a$10$mhfezzRvTWDvTgqQC49uZObZ1hyvnqF0hRFwRp2KR53q.kY0Z58v6');
 
-INSERT INTO inventory (user_id)
+INSERT INTO inventory (user_id, set_name, image_url, build_url)
 VALUES
-  (1), (1), (1), (1), (1), (1), (1), (1), (1), (1);
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/');
 
-INSERT INTO favorites (user_id) 
+INSERT INTO favorites (user_id, set_name, image_url, build_url)
 VALUES
-  (1), (1), (1), (1), (1), (1), (1), (1), (1), (1);
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/'), 
+  (1, 'Red Creatures', 'https://cdn.rebrickable.com/media/sets/31032-1.jpg', 'https://rebrickable.com/sets/31032-1/red-creatures/');
 
 COMMIT;
