10ba1d68520c501eb4f42a0ef33f5ca061aecbe5
Timothy Putzke <tsputzke@gmail.com>
2020-01-14 11:18:13 -0500

    Add rebrickable API connection

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { NODE_ENV } = require('./config')
 const UsersRouter = require('./users/users-router')
 const FavoritesRouter = require('./favorites/favorites-router')
 const InventoryRouter = require('./inventory/inventory-router')
+const rebrickable = require('./rebrickable')
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(cors())
 app.use('/api/users', UsersRouter)
 app.use('/api/favorites', FavoritesRouter)
 app.use('/api/inventory', InventoryRouter)
+app.use('/api/rebrickable', rebrickable)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
---
+++ src/rebrickable.js
@@ -0,0 +1,59 @@
+
+const express = require('express')
+const path = require('path')
+const fetch = require('node-fetch');
+
+const rebrickable = express.Router()
+
+// Return set info given a set id
+rebrickable
+  .route('/:searchTerm')
+  .get((req, res) => {
+    const searchTerm = req.params.searchTerm;
+    function handleSetSearch(searchTerm) {
+      fetch(`https://rebrickable.com/api/v3/lego/sets/${searchTerm}-1`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `key ${process.env.REBRICKABLE_API_KEY}`
+            }
+          })
+          // If call is successful
+          .then(response => response.json())
+          .then( response => {
+            response.set_num ? res.send(response) : res.send('Invalid set number, try again')
+          })
+          // If call fails
+          .catch(err => console.log(err))
+    }
+
+    handleSetSearch(searchTerm);
+    })
+
+// Return alternate builds given a set id
+rebrickable
+  .route('/alternates/:searchTerm')
+  .get((req, res) => {
+    const searchTerm = req.params.searchTerm;
+    function handleSetSearch(searchTerm) {
+      fetch(`https://rebrickable.com/api/v3/lego/sets/${searchTerm}-1/alternates`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `key ${process.env.REBRICKABLE_API_KEY}`
+            }
+          })
+          // If call is successful
+          .then(response => response.json())
+          .then( response => {
+            console.log(response)
+            response.count ? res.send(response) : res.send('Invalid set number, try again')
+          })
+          // If call fails
+          .catch(err => console.log(err))
+    }
+
+    handleSetSearch(searchTerm);
+    })
+
+module.exports = rebrickable
